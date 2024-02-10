6c4699d164dacd2b2dad27c362802078051d9927
Matheus Soares <69659304+msoareseq@users.noreply.github.com>
2024-02-10 00:12:14 -0300

    Improve word selection
    By removing punctuation marks from the text

--- src/powertxt/powertxt.py
+++ src/powertxt/powertxt.py
@@ -1,70 +1,92 @@
 from pathlib import Path
+import constants
 
 class PowerTxt:
     """A simple text handling class."""	
-    
+        
     # class methods
     def __init__(self, filename):
         self.path = Path(filename)
-        self.data = self.load()
+        self.text = self.load()
+        self.lines = self.text.splitlines()
+
+        for p in constants.P_MARKS:
+            text_no_marks = text_no_marks.replace(p, " ")
+        
+        self.words = text_no_marks.split()
 
     def __str__(self):
-        return self.data
-    
+        return self.text
+
     # public methods
 
+    # file management
     def load(self, filename):
         """Load a new text file."""
         self.path = Path(filename)
-        self.data = self.path.read_text().splitlines()
+        self.text = self.path.read_text().splitlines()
 
     def save(self):
         """Save the current text file."""
-        self.path.write_text(self.data)
+        self.path.write_text(self.text)
 
     def save_as(self, filename):
         """Save the current text file as a new file."""
         self.path = Path(filename)
         self.save()
 
+
+    # text manipulation
     def append_line(self, text):
         """Append new text line to the current file."""
-        self.data += '\n' + text + '\n'
+        self.text += '\n' + text + '\n'
 
     def get_line(self, line_number):
         """Get line from current text."""
-        if line_number < 0 or line_number >= len(self.data):
+        if line_number < 0 or line_number >= len(self.text):
             return None
         
-        return self.data[line_number]
+        return self.text[line_number]
     
     def set_line(self, line_number, text):
         """Set line in current text."""
-        if line_number < 0 or line_number >= len(self.data):
+        if line_number < 0 or line_number >= len(self.text):
             return
         
-        self.data[line_number] = text
+        self.text[line_number] = text
 
     def delete_line(self, line_number):
         """Delete line from current text."""
-        if line_number < 0 or line_number >= len(self.data):
+        if line_number < 0 or line_number >= len(self.text):
             return
         
-        del self.data[line_number]
+        del self.text[line_number]
 
     def get_all_words(self):
         """Get all words from current text."""
-        return self.data.split()
+        return self.words
     
     def get_unique_words(self):
         """Get unique words from current text."""
-        return set(self.data.split())
+        return set(self.words)
     
     def get_word_count(self):
         """Get word count from current text."""
-        return len(self.data.split())
+        return len(self.words)
     
     def get_line_count(self):
         """Get line count from current text."""
-        return len(self.data)
+        return len(self.lines)
+    
+    def get_top_x_words(self, min_lenght = 1):
+        """Get top x words from current text."""
+        word_count = {}
+        for word in self.words:
+            if len(word) >= min_lenght:
+                if word in word_count:
+                    word_count[word] += 1
+                else:
+                    word_count[word] = 1
+        
+        return sorted(word_count.items(), key=lambda x: x[1], reverse=True)
     

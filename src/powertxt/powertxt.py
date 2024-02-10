9392ed59a67ab9689b3b23a479cfcd8ac84deb7c
Matheus Soares <69659304+msoareseq@users.noreply.github.com>
2024-02-09 22:30:37 -0300

    Add line methods
    Add methods to control, get, count and set lines

--- src/powertxt/powertxt.py
+++ src/powertxt/powertxt.py
@@ -2,14 +2,21 @@ from pathlib import Path
 
 class PowerTxt:
     """A simple text handling class."""	
+    
+    # class methods
     def __init__(self, filename):
         self.path = Path(filename)
         self.data = self.load()
 
+    def __str__(self):
+        return self.data
+    
+    # public methods
+
     def load(self, filename):
         """Load a new text file."""
         self.path = Path(filename)
-        self.data = self.path.read_text()
+        self.data = self.path.read_text().splitlines()
 
     def save(self):
         """Save the current text file."""
@@ -20,4 +27,44 @@ class PowerTxt:
         self.path = Path(filename)
         self.save()
 
+    def append_line(self, text):
+        """Append new text line to the current file."""
+        self.data += '\n' + text + '\n'
+
+    def get_line(self, line_number):
+        """Get line from current text."""
+        if line_number < 0 or line_number >= len(self.data):
+            return None
+        
+        return self.data[line_number]
+    
+    def set_line(self, line_number, text):
+        """Set line in current text."""
+        if line_number < 0 or line_number >= len(self.data):
+            return
+        
+        self.data[line_number] = text
+
+    def delete_line(self, line_number):
+        """Delete line from current text."""
+        if line_number < 0 or line_number >= len(self.data):
+            return
+        
+        del self.data[line_number]
+
+    def get_all_words(self):
+        """Get all words from current text."""
+        return self.data.split()
+    
+    def get_unique_words(self):
+        """Get unique words from current text."""
+        return set(self.data.split())
+    
+    def get_word_count(self):
+        """Get word count from current text."""
+        return len(self.data.split())
+    
+    def get_line_count(self):
+        """Get line count from current text."""
+        return len(self.data)
     
